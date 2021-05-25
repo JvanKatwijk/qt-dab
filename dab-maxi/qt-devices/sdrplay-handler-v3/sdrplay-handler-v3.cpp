099df6a57cc3b5d22d489ec50851373296dbc44a
jan <J.vanKatwijk@gmail.com>
2021-05-25 19:22:30 +0200

    changes to sdr-v3 handler

--- dab-maxi/qt-devices/sdrplay-handler-v3/sdrplay-handler-v3.cpp
+++ dab-maxi/qt-devices/sdrplay-handler-v3/sdrplay-handler-v3.cpp
@@ -93,7 +93,7 @@ int	get_lnaGRdB (int hwVersion, int lnaState) {
 	            sdrplaySettings -> value ("sdrplay-lnastate", 4). toInt());
 
 	lnaState		=
-	            sdrplaySettings -> value ("sdrplay-lnastate", 4). toInt());
+	            sdrplaySettings -> value ("sdrplay-lnastate", 4). toInt();
 	ppmControl		-> setValue (
 	            sdrplaySettings -> value ("sdrplay-ppm", 0). toInt());
 
@@ -864,8 +864,11 @@ ULONG APIkeyValue_length = 255;
 	                    (LPDWORD)&APIkeyValue_length);
 //	Ok, make explicit it is in the 32/64 bits section
 	   wchar_t *x =
+#ifndef __BITS64__
 	        wcscat (APIkeyValue, (wchar_t *)L"\\x86\\sdrplay_api.dll");
-//	        wcscat (APIkeyValue, (wchar_t *)L"\\x64\\sdrplay_api.dll");
+#else
+	        wcscat (APIkeyValue, (wchar_t *)L"\\x64\\sdrplay_api.dll");
+#endif
 	   RegCloseKey(APIkey);
 
 	   Handle	= LoadLibrary (x);
