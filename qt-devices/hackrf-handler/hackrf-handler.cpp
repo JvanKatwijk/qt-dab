JvanKatwijk/qt-dab

commit 16199306a07a2550722b7305ad73149d90cb7d8b

added tpeg-client

qt-devices/hackrf-handler/hackrf-handler.cpp
@@ -361,7 +361,7 @@ int	res;
 //	The brave old getSamples. For the hackrf, we get
 //	size still in I/Q pairs
 int32_t	hackrfHandler::getSamples (std::complex<float> *V, int32_t size) { 
-std::complex<int8_t> temp [size];
+auto *temp = dynVec (std::complex<float>, size);
 	int amount      = _I_Buffer. getDataFromBuffer (temp, size);
 	for (int i = 0; i < amount; i ++)
 	   V [i] = std::complex<float> (real (temp [i]) / 127.0f,

ofdm-handler.cpp
@@ -4,7 +4,7 @@
  *    Jan van Katwijk (J.vanKatwijk@gmail.com)
  *    Lazy Chair Computing
  *
- *    This file is part of the Qt-DAB program
+ *    This file is part of Qt-DAB
  *
  *    Qt-DAB is free software; you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by

ofdm-handler.h
@@ -4,7 +4,7 @@
  *    Jan van Katwijk (J.vanKatwijk@gmail.com)
  *    Lazy Chair Computing
  *
- *    This file is part of the Qt-DAB program
+ *    This file is part of Qt-DAB
  *
  *    Qt-DAB is free software; you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by

qt-dab-RC/qt-dab-RC.pro
@@ -435,10 +435,10 @@ CONFIG		+= spyServer-8
 #CONFIG		+= elad-device
 #CONFIG		+= faad
 CONFIG		+= fdk-aac
-CONFIG		+= preCompiled
-#CONFIG		+= tiiLib
+#CONFIG		+= preCompiled
+CONFIG		+= tiiLib
 #very experimental, simple server for connecting to a tdc handler
-#CONFIG		+= datastreamer
+CONFIG		+= datastreamer
 #to handle output of embedded an IP data stream, uncomment
 #CONFIG		+= send_datagram
 

qt-dab-RC/radio.cpp
@@ -983,6 +983,7 @@ uint8_t localBuffer [length + 8];
 	   return;
 	}
 #ifdef	DATA_STREAMER
+	fprintf (stderr, "%d\n", length);
 	dataBuffer. getDataFromBuffer (&localBuffer [8], length);
 	localBuffer [0] = 0xFF;
 	localBuffer [1] = 0x00;

qt-devices/airspy-handler/airspy-handler.cpp
@@ -396,7 +396,8 @@ int32_t  i, j;
 	return 0;
 }
 //
-const char *airspyHandler::getSerial() {
+const
+char	*airspyHandler::getSerial () {
 airspy_read_partid_serialno_t read_partid_serialno;
 int result = my_airspy_board_partid_serialno_read (device,
 	                                          &read_partid_serialno);

qt-devices/lime-handler/lime-handler.cpp
@@ -1,10 +1,10 @@
 #
 /*
- *    Copyright (C) 2014 .. 2019
+ *    Copyright (C) 2014 .. 2023
  *    Jan van Katwijk (J.vanKatwijk@gmail.com)
  *    Lazy Chair Computing
  *
- *    This file is part of the Qt-DAB program
+ *    This file is part of Qt-DAB
  *
  *    Qt-DAB is free software; you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -239,7 +239,7 @@ int	res;
 	return true;
 }
 	
-void	limeHandler::stopReader() {
+void	limeHandler::stopReader	() {
 	close_xmlDump ();
 	if (!isRunning())
 	   return;
@@ -254,7 +254,7 @@ void	limeHandler::stopReader() {
 }
 
 int	limeHandler::getSamples	(std::complex<float> *V, int32_t size) {
-std::complex<int16_t> temp [size];
+auto *temp	= dynVec (std::complex<int16_t>, size);
 
         int amount      = _I_Buffer. getDataFromBuffer (temp, size);
 	if (filtering) {

commit 486455abc6234e12af5bf5e099bae4f2f4801d02

hackrf-handler: fix getSamples()
16199306a07a2550722b7305ad73149d90cb7d8b accidentally changed the
datatype of hackrf samples from complex<int8_t> to complex<float>. This
was then again changed to complex<int16_t> in
b3b83093e27bd4b272e5f10c1ffee28c753e254f. However, only complex<int8_t>
allows capturing valid samples from a HackRF.

qt-devices/hackrf-handler/hackrf-handler.cpp
@@ -365,7 +365,7 @@ int	res;
 //	The brave old getSamples. For the hackrf, we get
 //	size still in I/Q pairs
 int32_t	hackrfHandler::getSamples (std::complex<float> *V, int32_t size) { 
-auto *temp = dynVec (std::complex<int16_t>, size);
+auto *temp = dynVec (std::complex<int8_t>, size);
 	int amount      = _I_Buffer. getDataFromBuffer (temp, size);
 	for (int i = 0; i < amount; i ++)
 	   V [i] = std::complex<float> (real (temp [i]) / 127.0f,